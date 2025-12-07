7678af16f59c57f4df9850f3d8c485de8c4b5856
pakwoid <pakwoid@outlook.com>
2025-12-07 02:10:12 +0000

    add middleware for user permission

--- routers/userRouter.mjs
+++ routers/userRouter.mjs
@@ -1,12 +1,36 @@
 import express from 'express'
-import userController from '../controller/userController.mjs'
+
+import permissionHelper
+  from '../helpers/permissionHelper.mjs'
+
+import userController
+  from '../controller/userController.mjs'
 
 const userRouter = express.Router()
 
-userRouter.get('/view/all', userController.getViewAll)
-userRouter.get('/view/:id', userController.getViewId)
-userRouter.post('/create', userController.postCreate)
-userRouter.put('/update/:id', userController.putUpdateId)
-userRouter.delete('/remove/:id', userController.deleteRemoveId)
+userRouter.get('/view/all',
+  permissionHelper,
+  userController.getViewAll
+)
+
+userRouter.get('/view/:id',
+  permissionHelper,
+  userController.getViewId
+)
+
+userRouter.post('/create',
+  permissionHelper,
+  userController.postCreate
+)
+
+userRouter.put('/update/:id',
+  permissionHelper,
+  userController.putUpdateId
+)
+
+userRouter.delete('/remove/:id',
+  permissionHelper,
+  userController.deleteRemoveId
+)
 
 export default userRouter 
